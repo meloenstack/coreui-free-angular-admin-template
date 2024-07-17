052c080b0fe01b241e46476b29110cc21106b06c
meloenstack <carmelo.atienza@enstack.com>
2024-07-18 00:16:13 +0800

    feat: setup smart table

--- src/app/views/payments/remittance/pending/pending.component.ts
+++ src/app/views/payments/remittance/pending/pending.component.ts
@@ -1,12 +1,125 @@
-import { Component } from '@angular/core';
+import { RemittanceService } from './../remittance.service';
+import { Component, OnInit } from '@angular/core';
+import { TableModule, Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export interface Country {
+  name?: string;
+  code?: string;
+}
+
+export interface Representative {
+  name?: string;
+  image?: string;
+}
+
+export interface Customer {
+  id?: number;
+  name?: string;
+  country?: Country;
+  company?: string;
+  date?: string | Date;
+  status?: string;
+  activity?: number;
+  representative?: Representative;
+  verified?: boolean;
+  balance?: number;
+}
 
 @Component({
   selector: 'app-pending',
-  standalone: true,
-  imports: [],
   templateUrl: './pending.component.html',
-  styleUrl: './pending.component.scss'
+  standalone: true,
+  imports: [
+    TableModule,
+    TagModule,
+    IconFieldModule,
+    InputTextModule,
+    InputIconModule,
+    MultiSelectModule,
+    DropdownModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+  ],
+  providers: [RemittanceService],
 })
-export class PendingComponent {
+export class PendingComponent implements OnInit {
+  customers!: Customer[];
 
+  representatives!: Representative[];
+
+  statuses!: any[];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
+
+  constructor(private remittanceService: RemittanceService) {}
+
+  ngOnInit() {
+    this.remittanceService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      this.loading = false;
+
+      this.customers.forEach(
+        (customer) => (customer.date = new Date(<Date>customer.date))
+      );
+    });
+
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'Xuxue Feng', image: 'xuxuefeng.png' },
+    ];
+
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' },
+    ];
+  }
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'unqualified':
+        return 'danger';
+
+      case 'qualified':
+        return 'success';
+
+      case 'new':
+        return 'info';
+
+      case 'negotiation':
+        return 'warning';
+
+      case 'renewal':
+        return undefined;
+      default:
+        return 'success';
+    }
+  }
 }
